5b7afb8894b084df38c6b9fb15f67720fb9b9172
Andrew Wyper <andy_wyper@yahoo.co.uk>
2020-02-19 15:08:29 +0000

    mvp done

--- src/main/java/ShoppingBasket.java
+++ src/main/java/ShoppingBasket.java
@@ -1,11 +1,12 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ShoppingBasket {
-    private HashMap<String, Double> items;
+    private ArrayList<Item> items;
     private double subTotal;
 
     public ShoppingBasket(){
-        this.items = new HashMap();
+        this.items = new ArrayList<Item>();
         this.subTotal = 0;
     }
 
@@ -15,5 +16,24 @@ public class ShoppingBasket {
 
     public double getSubTotal() {
         return this.subTotal;
+    }
+
+    public void addItem(Item item) {
+        this.items.add(item);
+    }
+
+    public void removeItem(Item item) {
+        this.items.remove(item);
+    }
+
+    public void emptyBasket() {
+        this.items.clear();
+    }
+
+    public void calculateSubTotal(){
+        for (Item item : this.items){
+            this.subTotal += item.getPrice();
+        }
+
     }
 }
--- src/test/java/ShoppingBasketTest.java
+++ src/test/java/ShoppingBasketTest.java
@@ -5,10 +5,12 @@ import static org.junit.Assert.assertEquals;
 
 public class ShoppingBasketTest {
     ShoppingBasket shoppingBasket;
+    Item item;
 
     @Before
     public void before(){
         shoppingBasket = new ShoppingBasket();
+        item = new Item("Biscuits", 1.50);
     }
 
     @Test
@@ -20,4 +22,35 @@ public class ShoppingBasketTest {
     public void subTotalStartsAt0(){
         assertEquals(0, shoppingBasket.getSubTotal(), 0.01);
     }
+
+    @Test
+    public void canAddItem(){
+        shoppingBasket.addItem(item);
+        assertEquals(1, shoppingBasket.getNumberItems());
+    }
+
+    @Test
+    public void canRemoveItem(){
+        shoppingBasket.addItem(item);
+        shoppingBasket.addItem(item);
+        shoppingBasket.removeItem(item);
+        assertEquals(1, shoppingBasket.getNumberItems());
+    }
+
+    @Test
+    public void canClearBasket(){
+        shoppingBasket.addItem(item);
+        shoppingBasket.addItem(item);
+        shoppingBasket.emptyBasket();
+        assertEquals(0, shoppingBasket.getNumberItems());
+    }
+
+    @Test
+    public void canUpdateSubTotal(){
+        shoppingBasket.addItem(item);
+        shoppingBasket.addItem(item);
+        shoppingBasket.calculateSubTotal();
+        assertEquals(3.00, shoppingBasket.getSubTotal(), 0.01);
+    }
+
 }
